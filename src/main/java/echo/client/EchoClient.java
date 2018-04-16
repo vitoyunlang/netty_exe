81ea4f4be18aadf2ea1c6375c3bcf1290ef6ee65
vito <15502155219@163.com>
2018-04-16 21:16:40 +0800

    git init

---
+++ src/main/java/echo/client/EchoClient.java
@@ -0,0 +1,10 @@
+package echo.client;
+
+public class EchoClient {
+
+    private static final int port = 8080;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/main/java/echo/server/EchoServer.java
@@ -0,0 +1,4 @@
+package echo.server;
+
+public class EchoServer {
+}
